20834c03013b2300edcbf0238ff41e12a0f0a4b6
victorgxn <liseezbusiness@gmail.com>
2024-06-26 15:31:53 +0200

    Comando hacer pull de todos los archivos

---
+++ git_pull_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Navigate to the clickferry directory in the root
+cd ~/clickferry
+
+# Loop through each project directory and pull
+for project in */ ; do
+    echo "Pulling in $project"
+    cd "$project"
+    git pull
+    cd ..
+done
